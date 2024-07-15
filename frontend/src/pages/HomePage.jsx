400dee1115ed7370ef23903534675d990bfe3e02
Seneth Mendis <83528403+senethmendis@users.noreply.github.com>
2024-07-15 22:22:24 +0530

    filter added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { DummyProductData, DummySalesRow, subFilters } from "../constants";
 import ProductCard from "../components/ProductCard";
 import Button from "../components/Button";
@@ -6,12 +6,15 @@ import { plus, reporticon } from "../assets";
 import CartItemRow from "../components/CartItemRow";
 
 const HomePage = () => {
+  const [filter, setFilter] = useState("all");
+
   return (
     <section className="w-full h-full flex flex-row bg-parimary-gray">
       <div className="w-full h-full p-3">
         <div className="w-full items-center flex flex-row border border-gray-100 justify-evenly bg-white py-1 rounded-md mb-5 shadow-2xl">
           {subFilters.map((filter, i) => (
             <p
+              onClick={() => setFilter(filter)}
               key={i}
               className="px-2 capitalize cursor-pointer rounded-md hover:bg-orange-400 hover:border-orange-400 hover:text-white transition-all duration-150"
             >
@@ -21,7 +24,11 @@ const HomePage = () => {
         </div>
         <div className="w-full">
           <div className="grid grid-cols-4 overflow-y-scroll overflow-x-hidden h-[648px] gap-4 px-2 no-scrollbar">
-            {DummyProductData.map((product, i) => (
+            {DummyProductData.filter((product) => {
+              return filter.toLowerCase() === "all"
+                ? product
+                : product.category.toLowerCase().includes(filter);
+            }).map((product, i) => (
               <ProductCard
                 key={i}
                 title={product.title}
