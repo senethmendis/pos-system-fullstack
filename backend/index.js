9b3c1d66a36b2ae5867462968682956667dcfabb
Malitha Supun <malithasupun2@gmail.com>
2024-07-16 23:51:50 +0530

    Added dotenv dependency and configured it to load environment variables in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = 8080;
@@ -9,9 +10,10 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
+const mongoDBURI = process.env.MONGODB_URI;
 mongoose
   .connect(
-    "mongodb+srv://admin:admin@cluster0.4shrqxz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    mongoDBURI
   )
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
