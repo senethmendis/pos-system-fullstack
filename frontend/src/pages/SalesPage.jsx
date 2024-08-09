d364c093a374ab388f92ea010e438ef6bf6bfd87
senethmendis <mendis.seneth@gmail.com>
2024-08-10 00:32:20 +0530

    "Updated SalesPage component to return a div with class and text content"

--- frontend/src/pages/SalesPage.jsx
+++ frontend/src/pages/SalesPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SalesPage = () => {
-  return <div>SalesPage</div>;
+  return (
+    <div className="w-full h-full flex flex-col bg-parimary-gray px-5">
+      SalesPage
+    </div>
+  );
 };
 
 export default SalesPage;
